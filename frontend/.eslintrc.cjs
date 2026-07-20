589f8dae03c36b2139896fbd8e5043dd6c28e18f
Reina_Visiy <reinavisiy@gmail.com>
2026-07-20 22:13:23 +0000

    chore(frontend): ajoute la configuration ESLint manquante
    Le script 'npm run lint' echouait car aucun fichier .eslintrc n'etait
    present. Ajout d'une config standard React 18 + Vite (eslint:recommended,
    plugin:react/recommended, react-hooks, react-refresh).

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,22 @@
+// Configuration ESLint pour le frontend AgryCam (React 18 + Vite).
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'preview.html', 'build_preview.js'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.3' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off',
+  },
+};
